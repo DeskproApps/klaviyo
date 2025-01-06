14099219ebdf6415d692909a9c43d6302802d43b
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2025-01-06 15:44:00 +0000

    [Chore] SC-162578 SC-123527 upgrade deps

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import type { To, ParamKeyValuePair } from "react-router-dom";
 import type { DropdownValueType } from "@deskpro/deskpro-ui";
-import type { Context, IDeskproClient, V2ProxyRequestInitBody } from "@deskpro/app-sdk";
+import type { Context, IDeskproClient } from "@deskpro/app-sdk";
 import type { Response } from "./services/klaviyo/types";
 
 /** Common types */
@@ -20,7 +20,7 @@ export type RequestParams = {
   url?: string;
   rawUrl?: string;
   method?: ApiRequestMethod;
-  data?: Dict<string>|RequestInit["body"]|V2ProxyRequestInitBody["body"];
+  data?: Dict<string>|RequestInit["body"];
   headers?: Dict<string>;
   queryParams?: string|Dict<string>|ParamKeyValuePair[];
   settings?: Settings;
@@ -30,9 +30,6 @@ export type Request = <Data, Include = undefined>(
   client: IDeskproClient,
   params: RequestParams,
 ) => Response<Data, Include>;
-
-// V2ProxyRequestInit
-export type FetchOptions = Pick<RequestParams, "method"|"headers"> & V2ProxyRequestInitBody;
 
 /** Deskpro types */
 export type Settings = {
